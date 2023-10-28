97745c48bc789fbfd61dbb0ed69b845fa7a3dda9
prnvrain <pranavnair78@gmail.com>
2023-10-28 21:38:27 +0530

    change 5

--- test.py
+++ test.py
@@ -296,5 +296,5 @@ if __name__ == "__main__":
     model.to(device)
     image_paths = get_images(args.data_path)
 
-    padcrop(image_paths, model, postprocessors, device, args.output_dir,50)
+    padcrop(image_paths, model, postprocessors, device, args.output_dir,0)
 
